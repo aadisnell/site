882039cf186e63a63155d01f20351b3cbed32111
Charles <asirificharles@yahoo.com>
2017-09-09 16:25:20 +0100

    personal site added

---
+++ js/script.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+$(function(){
+
+$("#navbarToggle").blur(function(event){
+var screenWidth = window.innerWidth;
+
+if (screenWidth <768){
+
+  $("#collapsable-nav").collapse('hide');
+}
+
+});
+
+$("#navbarToggle").click(function(event){
+  $(event.target).focus();
+})
+
+});
+=======
+$(function(){
+
+$("#navbarToggle").blur(function(event){
+var screenWidth = window.innerWidth;
+
+if (screenWidth <768){
+
+  $("#collapsable-nav").collapse('hide');
+}
+
+});
+
+$("#navbarToggle").click(function(event){
+  $(event.target).focus();
+})
+
+});
+>>>>>>> 191bcc7cd16e5544df155599c810d78dface899f
